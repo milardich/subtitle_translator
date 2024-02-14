3b8c990856ae70645fac34ce2530d95453aeee11
milardich <stjepan.milardic1@gmail.com>
2024-02-14 17:29:07 +0100

    refactor

--- srt_translator.py
+++ srt_translator.py
@@ -7,12 +7,14 @@ import threading
 import getopt
 
 
+
 translate_settings = {
     "threaded": False,
     "language": "en",
     "directories": [],
     "srtFiles": []
 }
+
 
 
 def translate_text(
@@ -30,10 +32,10 @@ def translate_text(
         target_language_code = target_language,
         parent = parent,
         mime_type = "text/plain",
-        #source_language_code = "en",
         model = model_path
     )
     return response
+
 
 
 def load_data(srt_file_path):
@@ -41,9 +43,11 @@ def load_data(srt_file_path):
     return srt_file.read()
 
 
+
 def single_line_text(text: str) -> str:
     split_text = text.split('\n')
     return ' '.join(split_text)
+
 
 
 def multi_line_text(text: str):
@@ -58,6 +62,7 @@ def multi_line_text(text: str):
     return result
 
 
+
 def translate_subtitle(subtitle, destination_language):
     if isinstance(subtitle, bytes):
         subtitle = subtitle.decode("utf-8")
@@ -65,14 +70,15 @@ def translate_subtitle(subtitle, destination_language):
     result = translate_text(text = subtitle, target_language = destination_language)
     translated_result = result.translations[0].translated_text
     translated_result = multi_line_text(translated_result)
-    print(f"\n\n{subtitle}\n--------------------\n{translated_result}\n\n")
     return translated_result
     
+
 
 def save_translated_srt(subtitles, file_name):
     srt_file = open(file_name, "a", encoding="utf-16")
     composed_srt = srt.compose(subtitles)
     srt_file.write(composed_srt)
+
 
 
 # destination_language -> language code to translate to
@@ -88,30 +94,52 @@ def translate_srt_file(srt_file_path, destination_language, translated_srt_path)
     print(f"\n[TRANSLATED ({time_end - time_start})]: {srt_file_path}\n")
 
 
+
 def dir_translate(directories: list[str]):
     for directory in directories:
-        try:
-            files = os.listdir(directory)
-            for file in files:
-                if file.endswith(".srt"):
-                    translated_srt_path = directory + "\\"+ file[:-4] + "." + translate_settings["language"] + ".srt"
-                    if(sys.argv.__contains__("-t")):
-                        t = threading.Thread(target = translate_srt_file, args = (directory + "\\" + file, translate_settings["language"], translated_srt_path))
-                        t.start()
-                    else:
-                        translate_srt_file(directory + "\\" + file, translate_settings["language"], translated_srt_path)
-        except NotADirectoryError as err:
-            print(err)
+        files = os.listdir(directory)
+        for file in files:
+            if file.endswith(".srt"):
+                translated_srt_path = directory + "\\"+ file[:-4] + "." + translate_settings["language"] + ".srt"
+                if(sys.argv.__contains__("-t")):
+                    t = threading.Thread(
+                        target = translate_srt_file, 
+                        args = (
+                            directory + "\\" + file, 
+                            translate_settings["language"], 
+                            translated_srt_path
+                        )
+                    )
+                    t.start()
+                else:
+                    translate_srt_file(
+                        directory + "\\" + file, 
+                        translate_settings["language"], 
+                        translated_srt_path
+                    )
+       
 
 
 def srt_translate(srt_files: list[str]):
     for srt_file in srt_files:
         translated_srt_path = srt_file[:-4] + "." + translate_settings["language"] + ".srt"
         if translate_settings["threaded"] == True:
-            t = threading.Thread(target = translate_srt_file, args = (srt_file, translate_settings["language"], translated_srt_path))
+            t = threading.Thread(
+                target = translate_srt_file, 
+                args = (
+                    srt_file, 
+                    translate_settings["language"], 
+                    translated_srt_path
+                )
+            )
             t.start()
         else:
-            translate_srt_file(srt_file, translate_settings["language"], translated_srt_path)
+            translate_srt_file(
+                srt_file, 
+                translate_settings["language"], 
+                translated_srt_path
+            )
+
 
 
 # TODO: implement BCP-47 language code validation
@@ -124,6 +152,8 @@ Example usage:
 > python srt_translator.py -t --l=hr --d="D:\Movies\ExampleMovie","D:\Movies\ExampleMovie"
 > python srt_translator.py -t --l=hr --srt="D:\Movies\ExampleMovie\subtitle.srt","D:\Movies\ExampleMovie\subtitle2.srt"
 '''
+
+
 
 if __name__ == "__main__":
     
@@ -153,40 +183,3 @@ if __name__ == "__main__":
 
     if len(translate_settings["srtFiles"]) > 0:
         srt_translate(translate_settings["srtFiles"])
-
-
-############################
-    # print(f"\nTranslation started at {time.asctime(time.localtime())}")
-
-    # if(sys.argv.__contains__("-t")):
-    #     print(f"\nThreading: ON\n")
-
-    # # Translate every srt file in provided directory
-    # if sys.argv[1] == "-d":
-    #     directory = sys.argv[4]
-    #     destination_language = sys.argv[1]
-    #     files = os.listdir(directory)
-    #     for file in files:
-    #         if file.endswith(".srt"):
-    #             translated_srt_path = directory + "\\"+ file[:-4] + "." + destination_language + ".srt"
-    #             if(sys.argv.__contains__("-t")):
-    #                 t = threading.Thread(target = translate_srt_file, args = (directory + "\\" + file, destination_language, translated_srt_path))
-    #                 t.start()
-    #                 print(f"\nNew thread started")
-    #             else:
-    #                 translate_srt_file(directory + "\\" + file, destination_language, translated_srt_path)
-
-    # # Provide multiple paths of srt files as command line arguments
-    # else:
-    #     srt_files = sys.argv[1:]
-    #     destination_language = sys.argv[1]
-    #     for srt_file in srt_files:
-    #         if srt_file == "-t":
-    #             continue
-    #         translated_srt_path = srt_file[:-4] + "_-TRANSLATED.srt"
-    #         if(sys.argv.__contains__("-t")):
-    #             t = threading.Thread(target = translate_srt_file, args = (srt_file, destination_language, translated_srt_path))
-    #             t.start()
-    #         else:
-    #             print(f"\nThreading: OFF\n")
-    #             translate_srt_file(srt_file, destination_language, translated_srt_path)
